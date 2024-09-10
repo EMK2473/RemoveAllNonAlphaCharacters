29c6038d0f2070331a38aae2749889d001351e18
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-09 23:35:46 -0400

    declared and assigned variables; looped through input string; check if index is character; print characters that pass; format output and close scanner

--- RemoveAllNonAlphaCharacters.java
+++ RemoveAllNonAlphaCharacters.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class RemoveAllNonAlphaCharacters {
    public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String inputString = scnr.nextLine();
       
+      for(int i = 0; i < inputString.length(); ++i){
+         if(Character.isLetter(inputString.charAt(i))){
+            System.out.println(inputString.charAt(i));
+         }
+      }
+      System.out.println();
+      scnr.close();
    }
 }
